4321802878a7813d4e5ca44952bd741cf0459392
Michael <michael@andersen.net>
2022-02-14 01:16:35 +0400

    update: class MainActivity.kt

--- app/src/main/java/dev/m13d/myapplication/MainActivity.kt
+++ app/src/main/java/dev/m13d/myapplication/MainActivity.kt
@@ -2,10 +2,15 @@ package dev.m13d.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import dev.m13d.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView()
-    }
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    lateinit var binding: ActivityMainBinding
+
+//    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+//        super.onCreate(savedInstanceState, persistentState)
+//    } // It doesn't necessary to override method 'onCreate' to show bound layout if placed layout
+//    into AppCompatActivity constructor.
+//    Lesson 2 time 25:30.
 }
